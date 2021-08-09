35a3a3d55dc2d86305fe2253ffbe38e98b2fb225
James A. Brannan <jamesabrannan@hushmail.com>
2021-08-08 22:41:21 -0400

    initial commit

---
+++ dotnet-client-examples/PdfInfoExample.cs
@@ -0,0 +1,24 @@
+﻿using DynamicPDF.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_client_examples
+{
+    public static class PdfInfoExample
+    {
+        // simple example from Getting Started - pdf-info
+        public static void PdfInfoExampleOne(String key)
+        {
+            PdfResource resource = new PdfResource(@"c:/holding/pdf-text/fw4.pdf");
+            PdfInfo pdfInfo = new PdfInfo(resource);
+            pdfInfo.ApiKey = key;
+            PdfInfoResponse response = pdfInfo.Process();
+
+            Console.WriteLine(PrettyPrintUtil.JsonPrettify(response.JsonContent));
+            Console.WriteLine("=========================================");
+            Console.WriteLine(response.Content.Creator);
+
+        }
+    }
+}
--- dotnet-client-examples/Program.cs
+++ dotnet-client-examples/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using DynamicPDF.Api;
 
 namespace dotnet_client_examples
 {
+   
+
+    // NEVER INCLUDE YOUR KEY VALUE IN A GITHUB REPOSITORY.
+    // Assumes the base url of api.dynamicpdf.com
+
     class Program
     {
+        // replace with your key, only for debug use, you should always keep keys safe.
+        static String key = "DP.poEtD7F5tD1Ulp3qPcolUFaCcQFxWOvuNUqm/WragUdOSaAesnu3L6XE";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PdfInfoExample.PdfInfoExampleOne(key);
         }
     }
 }
