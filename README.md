3526d76b8e5e01c5e474a59715f73bf1e241e64f
James A. Brannan <jamesabrannan@hushmail.com>
2021-08-30 21:35:47 -0400

    changes to readme and instructions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotnet-client-examples
+
+The project contains a Visual Studio C# Console App (.NET Core) command-line project. The man class, `Program`, contains examples illustrating using the `pdf-info`, `image-info`, `pdf-text`, `pdf-xmp`, `dlex-layout`, and `pdf` endpoints.  Each endpoint example is in its own respective class. There are also examples illustrating using the instructions schema to create several PDFs using the `pdf`endpoint. These examples are in the `InstructionsExample` class.
+
+## Resources
+
+The needed local resources are contained in the `resources` folder in this project. The needed resources in the DynamicPdf Cloud API are contained in the `samples/shared` sub-folders.
+
+![](./dotnet-client-examples/resources/cloudapi-screenshot.png)
+
+
+
--- dotnet-client-examples/Program.cs
+++ dotnet-client-examples/Program.cs
@@ -13,18 +13,18 @@ namespace DynamicPdfCloudApiClientExamples
         static void Main(string[] args)
         {
 
-           // PdfInfoExample.PdfInfoExampleOne(args[0], args[1]);
-           // PrintDivider();
-           // ImageInfoExample.ImageInfoExampleOne(args[0], args[1]);
-           // PrintDivider();
-           // ImageInfoExample.ImageInfoExampleTwo(args[0], args[1]);
-           // PrintDivider();
-          //  PdfTextExample.PdfInfoExampleOne(args[0], args[1]);
-          //  PrintDivider();
-          //  PdfXmpExample.PdfXmpExampleOne(args[0], args[1]);
-          //  PdfExample.PdfExampleOne(args[0], args[1]);
-          //  PrintDivider();
-           // DlexLayoutExample.DlexLayoutExampleOne(args[0], args[1]);
+            PdfInfoExample.PdfInfoExampleOne(args[0], args[1]);
+            PrintDivider();
+            ImageInfoExample.ImageInfoExampleOne(args[0], args[1]);
+            PrintDivider();
+            ImageInfoExample.ImageInfoExampleTwo(args[0], args[1]);
+            PrintDivider();
+            PdfTextExample.PdfInfoExampleOne(args[0], args[1]);
+            PrintDivider();
+            PdfXmpExample.PdfXmpExampleOne(args[0], args[1]);
+            PdfExample.PdfExampleOne(args[0], args[1]);
+            PrintDivider();
+            DlexLayoutExample.DlexLayoutExampleOne(args[0], args[1]);
             InstructionsExample.DemoInstructions(args);
         }
 
