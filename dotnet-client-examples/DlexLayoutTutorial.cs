d147df2092d00860f2fb578ea2b50f59288fbed9
James A. Brannan <jamesabrannan@hushmail.com>
2021-11-15 20:42:05 -0500

    changes

---
+++ dotnet-client-examples/DlexLayoutTutorial.cs
@@ -0,0 +1,27 @@
+﻿using DynamicPDF.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DynamicPdfCloudApiClientExamples
+{
+    class DlexLayoutTutorial
+    {
+        public static void DlexLayoutExampleOne(String apiKey)
+        {
+            LayoutDataResource layoutData = new LayoutDataResource(@"C:/dynamicpdf/getting-started-examples/getting-started-data.json");
+            DlexLayout dlexEndpoint = new DlexLayout("samples/shared/dlex/getting-started.dlex", layoutData);
+            dlexEndpoint.ApiKey = apiKey;
+            PdfResponse response = dlexEndpoint.Process();
+            if (response.ErrorJson == null)
+            {
+                File.WriteAllBytes(@"C:/dynamicpdf/getting-started-examples/dlex-output.pdf", (byte[])response.Content);
+            }
+            else
+            {
+                Console.WriteLine(PrettyPrintUtil.JsonPrettify(response.ErrorJson));
+            }
+        }
+    }
+}
