edf2caebbca3d58aa709fb57f0513446e8df5a9c
James A. Brannan <jamesabrannan@hushmail.com>
2021-12-08 15:51:35 -0500

    changes

---
+++ MergeMultiplePdfs/Program.cs
@@ -0,0 +1,42 @@
+﻿using DynamicPDF.Api;
+using System;
+using System.IO;
+
+namespace MergeMultiplePdfs
+{
+    class Program
+    {
+    
+ static void Main(string[] args)
+        {
+            Run("DP.jNFADSRTMGk60fv4+QY1qID9bzpp+mrkC8IU8wcWtl2wSYcQFV1S3Mww", "C:/temp/dynamicpdf-api-samples/");
+        }
+
+        public static void Run(String apiKey, String basePath)
+        {
+            // create new pdf instance and set api key
+            Pdf pdf = new Pdf();
+            pdf.ApiKey = apiKey;
+
+            // add pdfs to merge from the cloud
+            pdf.AddPdf("samples/merge-multiple-pdfs/DocumentA.pdf");
+            pdf.AddPdf("samples/merge-multiple-pdfs/DocumentB.pdf");
+
+            // add pdf from local file system
+            PdfResource pdfResource = new PdfResource(basePath + "DocumentC.pdf");
+            pdf.AddPdf(pdfResource);
+
+            PdfResponse pdfResponse = pdf.Process();
+
+            // if the response is successful, save merged pdf to file. if error, then printout JSON error
+
+            if(pdfResponse.IsSuccessful)
+            {
+                File.WriteAllBytes(Path.Combine(basePath, "merge-multiple-documents-output.pdf"), pdfResponse.Content);
+            } else
+            {
+                Console.WriteLine(pdfResponse.ErrorJson);
+            }
+        }
+    }
+}
