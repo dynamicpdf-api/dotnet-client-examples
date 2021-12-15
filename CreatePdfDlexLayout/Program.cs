2c76bb036eb8e740d7405807886a4bd74bbbda57
James A. Brannan <jamesabrannan@hushmail.com>
2021-12-15 15:20:33 -0500

    changes

---
+++ CreatePdfDlexLayout/Program.cs
@@ -0,0 +1,32 @@
+﻿using DynamicPDF.Api;
+using System;
+using System.IO;
+
+namespace CreatingPdfDlexLayout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run("DP.7vATWolKJ4xdaefbf/pTgSW7uGWofsZAKctZ1J/hzV9yTrzDvmDI1lwT", "C:/temp/dynamicpdf-api-samples/creating-pdf-dlex-layout-endpoint/");
+        }
+
+        public static void Run(String apiKey, String basePath)
+        {
+            {
+                LayoutDataResource layoutData = new LayoutDataResource(basePath + "create-pdf-dlex-layout.json");
+                DlexLayout dlexEndpoint = new DlexLayout("samples/creating-pdf-dlex-layout-endpoint/create-pdf-dlex-layout.dlex", layoutData);
+                dlexEndpoint.ApiKey = apiKey;
+                PdfResponse response = dlexEndpoint.Process();
+                if (response.IsSuccessful)
+                {
+                    File.WriteAllBytes(basePath + "create-pdf-dlex-output.pdf", (byte[])response.Content);
+                }
+                else
+                {
+                    Console.WriteLine(response.ErrorJson);
+                }
+            }
+        }
+    }
+}
---
+++ ExtractingText/Program.cs
@@ -0,0 +1,32 @@
+﻿using DynamicPDF.Api;
+using System;
+
+namespace ExtractingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run("DP.NKSoPxiwOgZoypSVYaXyEARo2cO9Kk5BRgY2ZRC0jF/KQq4pDzhfK8yO", "C:/temp/dynamicpdf-api-samples/extract-text/");
+        }
+
+        public static void Run(String apiKey, String basePath)
+        {
+            PdfResource resource = new PdfResource(basePath + "fw4.pdf");
+            PdfText pdfText = new PdfText(resource);
+            pdfText.ApiKey = apiKey;
+            pdfText.StartPage = 1;
+            pdfText.PageCount = 2;
+
+            PdfTextResponse response = pdfText.Process();
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine((response.JsonContent));
+            } else
+            {
+                Console.WriteLine(response.ErrorJson);
+            }
+
+        }
+    }
+}
