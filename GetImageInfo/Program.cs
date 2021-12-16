58218bb07fe98ec1a849bc30cb37c60e4541357b
James A. Brannan <jamesabrannan@hushmail.com>
2021-12-15 21:14:08 -0500

    changes

---
+++ GetImageInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using DynamicPDF.Api;
+using System;
+
+namespace GetImageInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run("DP.NKSoPxiwOgZoypSVYaXyEARo2cO9Kk5BRgY2ZRC0jF/KQq4pDzhfK8yO", "c:/temp/dynamicpdf-api-samples/get-image-info/");
+        }
+
+        public static void Run(String apiKey, String basePath)
+        {
+            String key = apiKey;
+            ImageResource imageResource = new ImageResource(basePath + "dynamicpdflogo.png");
+            ImageInfo imageInfo = new ImageInfo(imageResource);
+            imageInfo.ApiKey = key;
+            ImageResponse response = imageInfo.Process();
+
+            if(response.IsSuccessful)
+            {
+                Console.WriteLine(response.JsonContent);
+            } else
+            {
+                Console.WriteLine(response.ErrorJson);
+            }
+        }
+
+    }
+}
---
+++ GetPdfInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using DynamicPDF.Api;
+using System;
+
+namespace GetPdfInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run("DP.NKSoPxiwOgZoypSVYaXyEARo2cO9Kk5BRgY2ZRC0jF/KQq4pDzhfK8yO", "c:/temp/dynamicpdf-api-samples/get-pdf-info/");
+        }
+
+        public static void Run(String apiKey, String basePath)
+        {
+            PdfResource resource = new PdfResource(basePath + "fw4.pdf");
+            PdfInfo pdfInfo = new PdfInfo(resource);
+            pdfInfo.ApiKey = apiKey;
+            PdfInfoResponse response = pdfInfo.Process();
+
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine(response.JsonContent);
+            } else
+            {
+                Console.WriteLine(response.ErrorJson);
+            }
+        }
+    }
+}
