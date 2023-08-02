287ea69634170bd4fd76b560eea0d57ff9cc82b0
James A. Brannan <jamesabrannan@hushmail.com>
2023-08-01 21:51:53 -0400

    dlex layout with object example

---
+++ UsersGuidePdfInstructions/SimpleReport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UsersGuidePdfInstructions
+{
+    class SimpleReport
+    {
+
+        public String ReportCreatedFor { get; set; }
+        public List<Product> Products { get; set; }
+     }
+}
