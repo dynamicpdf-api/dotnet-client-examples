281a008a637ae8c2d86bd3d2db5d4b9b360cc9ee
James A. Brannan <jamesabrannan@hushmail.com>
2024-01-02 13:41:01 -0500

    changes for local dlex-layout

--- DynamicPdfClientLibraryExamples/Examples/DlexLayoutExample.cs
+++ DynamicPdfClientLibraryExamples/Examples/DlexLayoutExample.cs
@@ -9,6 +9,12 @@ namespace DynamicPdfClientLibraryExamples.Examples
 
 		public static void Run(string apiKey, string basePath, string outputPath)
 		{
+			RunFromCloud(apiKey, basePath, outputPath);
+			RunFromLocal(apiKey, basePath, outputPath);
+		}
+
+		public static void RunFromCloud(string apiKey, string basePath, string outputPath)
+		{
 			LayoutDataResource layoutData = new LayoutDataResource(basePath + "SimpleReportWithCoverPage.json");
 			DlexLayout dlexEndpoint = new DlexLayout("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layoutData);
 			dlexEndpoint.ApiKey = apiKey;
@@ -17,6 +23,25 @@ namespace DynamicPdfClientLibraryExamples.Examples
 			if (response.IsSuccessful)
 			{
 				File.WriteAllBytes(outputPath + "/csharp-dlex-layout-example-output.pdf", response.Content);
+			}
+			else
+			{
+				Console.WriteLine(response.ErrorJson);
+			}
+		}
+
+		public static void RunFromLocal(string apiKey, string basePath, string outputPath)
+		{
+			LayoutDataResource layoutData = new LayoutDataResource(basePath + "SimpleReportWithCoverPage.json");
+			DlexResource dlexResource = new DlexResource(basePath + "SimpleReportWithCoverPage.dlex", "SimpleReportWithCoverPage.dlex");
+			DlexLayout dlexEndpoint = new DlexLayout(dlexResource , layoutData);
+			dlexEndpoint.AddAdditionalResource(basePath + "NorthwindLogo.gif", "NorthwindLogo.gif");
+			dlexEndpoint.ApiKey = apiKey;
+			PdfResponse response = dlexEndpoint.Process();
+
+			if (response.IsSuccessful)
+			{
+				File.WriteAllBytes(outputPath + "/csharp-dlex-layout-local-example-output.pdf", response.Content);
 			}
 			else
 			{
