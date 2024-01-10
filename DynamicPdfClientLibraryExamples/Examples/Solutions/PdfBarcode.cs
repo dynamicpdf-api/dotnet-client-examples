ec8b49607faf4911712f909031cca21c30c0a79e
James A. Brannan <jamesabrannan@hushmail.com>
2024-01-10 16:49:09 -0500

    Create PdfBarcode.cs

---
+++ DynamicPdfClientLibraryExamples/Examples/Solutions/PdfBarcode.cs
@@ -0,0 +1,39 @@
+﻿using DynamicPDF.Api;
+using DynamicPDF.Api.Elements;
+using System;
+using System.IO;
+
+namespace DynamicPdfClientLibraryExamples.Examples.Solutions
+{
+    class PdfBarcode
+    {
+        public static void Run(string apiKey, string outputPath)
+        {
+            Pdf pdf = new Pdf();
+            pdf.ApiKey = apiKey;
+
+            PageInput pageInput = pdf.AddPage(1008, 612);
+            pdf.Inputs.Add(pageInput);
+
+            Code11BarcodeElement code11BarcodeElement = new Code11BarcodeElement("12345678", ElementPlacement.TopCenter, 200, 50, 50);
+            code11BarcodeElement.Color = RgbColor.Red;
+
+            pageInput.Elements.Add(code11BarcodeElement);
+
+
+            Console.WriteLine(pdf.GetInstructionsJson(true));
+
+
+            PdfResponse pdfResponse = pdf.Process();
+
+            if (pdfResponse.IsSuccessful)
+            {
+                File.WriteAllBytes(outputPath + "/create-barcode-output.pdf", pdfResponse.Content);
+            }
+            else
+            {
+                Console.WriteLine(pdfResponse.ErrorJson);
+            }
+        }
+    }
+}
