348c501c20d2e81dc7f3fbba16a54ec30eedb378
James A. Brannan <jamesabrannan@hushmail.com>
2024-01-24 16:59:09 -0500

    delete pages

---
+++ DynamicPdfClientLibraryExamples/Examples/Solutions/DeletePages.cs
@@ -0,0 +1,54 @@
+﻿using DynamicPDF.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPdfClientLibraryExamples.Examples.Solutions
+{
+    class DeletePages
+    {
+        public static void Run(string apiKey, string basePath, string outputPath)
+        {
+            PerformDelete(apiKey, basePath, outputPath);
+        }
+
+        static void PerformDelete(string apiKey, string basePath, string outputPath)
+        {
+
+            //add pdf inputs
+
+            Pdf pdf = new Pdf();
+            pdf.Author = "Test Author";
+            pdf.Creator = "Test Creator";
+
+            PdfInput input = pdf.AddPdf(new PdfResource(basePath + "pdfnumberedinput.pdf"));
+            input.StartPage = 1;
+            input.PageCount = 3;
+
+            PdfInput input2 = pdf.AddPdf(new PdfResource(basePath + "pdfnumberedinput.pdf"));
+            input2.StartPage = 6;
+            input2.PageCount = 2;
+
+            
+            // output combined pdf
+
+            pdf.ApiKey = apiKey;
+
+
+            PdfResponse response = pdf.Process();
+
+
+            if (response.ErrorJson != null)
+            {
+                Console.WriteLine(Utility.PrettyPrintUtil.JsonPrettify(response.ErrorJson));
+            }
+            else
+            {
+                File.WriteAllBytes(outputPath + "/delete-pages-output.pdf", response.Content);
+            }
+        }
+    }
+}
