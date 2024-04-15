94422f7904f7d748c331bc872f39615b64dbbffe
James A. Brannan <jamesabrannan@hushmail.com>
2024-04-15 17:43:49 -0400

    removed key

--- ExtractPdfText/Program.cs
+++ ExtractPdfText/Program.cs
@@ -8,7 +8,7 @@ namespace ExtractPdfText
 
         private const string basePath = "../DynamicPdfClientLibraryExamples/Resources/extract-text-pdf-text-endpoint";
 
-        private const string apiKey = "DP.aKwxGlX97JUI0CubONSkXSKcebvWkkGmmQfz1994AkFYSZt64EQXjH/S";
+        private const string apiKey = "DP--api-key--";
 
         static void Main(string[] args)
         {
