161ebe7d89db281864773951131545a3d9bc743e
James A. Brannan <jamesabrannan@hushmail.com>
2024-06-25 11:03:52 -0400

    addbookmarks tutorial update

---
+++ AddBookmarksTutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using DynamicPdfClientLibraryExamples.Examples;
+using DynamicPdfClientLibraryExamples.Utility;
+using System;
+
+namespace AddBookmarksTutorial
+{
+    class Program
+    {
+        private const string basePath = "../DynamicPdfClientLibraryExamples/Resources/add-bookmarks/";
+        private const string outputPath = "Output";
+        private const string apiKey = "DP--api-key--";
+
+        static void Main(string[] args)
+        {
+            FileUtility.CreatePath(outputPath);
+            AddBookmarks.Run(apiKey, FileUtility.GetPath(basePath), FileUtility.GetPath(outputPath));
+        }
+    }
+}
