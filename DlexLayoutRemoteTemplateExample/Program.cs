21abb9b267bc70026d6a134bbc4bfee03169ba2b
James A. Brannan <jamesabrannan@hushmail.com>
2025-02-10 14:47:55 -0500

    Update Program.cs

--- DlexLayoutRemoteTemplateExample/Program.cs
+++ DlexLayoutRemoteTemplateExample/Program.cs
@@ -2,6 +2,7 @@
 using DynamicPdfClientLibraryExamples.Utility;
 using System;
 using System.IO;
+using System.Net;
 
 namespace DlexLayoutRemoteTemplateExample
 {
@@ -11,24 +12,29 @@ namespace DlexLayoutRemoteTemplateExample
 		private const string outputPath = "Output/";
 		private const string apiKey = "DP--api-key--";
 
+		private const string static_template = "https://github.com/dynamicpdf-api/dotnet-client-examples/blob/main/DlexLayoutRemoteTemplateExample/Resources/template_example.pdf";
+
 		static void Main(string[] args)
         {
 			FileUtility.CreatePath(outputPath);
-			RunFromLocal(apiKey, basePath, outputPath);
+			RunFromLocalWithRemoteTemplate(apiKey, basePath, outputPath);
         }
 
-		public static void RunFromLocal(string apiKey, string basePath, string outputPath)
+		public static void RunFromLocalWithRemoteTemplate(string apiKey, string basePath, string outputPath)
 		{
 			LayoutDataResource layoutData = new LayoutDataResource(FileUtility.GetPath(basePath + "ExampleTemplate.json"));
 			DlexResource dlexResource = new DlexResource(FileUtility.GetPath(basePath + "ExampleTemplate.dlex"), "ExampleTemplate.dlex");
 			DlexLayout dlexEndpoint = new DlexLayout(dlexResource, layoutData);
+			WebClient myWebClient = new WebClient();
+			byte[] myDataBuffer = myWebClient.DownloadData(static_template);
+
 			dlexEndpoint.AddAdditionalResource(FileUtility.GetPath(basePath + "template_example.pdf"), "template_example.pdf");
 			dlexEndpoint.ApiKey = apiKey;
 			PdfResponse response = dlexEndpoint.Process();
 
 			if (response.IsSuccessful)
 			{
-				File.WriteAllBytes(FileUtility.GetPath(outputPath + "local-example-output.pdf"), response.Content);
+				File.WriteAllBytes(FileUtility.GetPath(outputPath + "local-remote-template-example-output.pdf"), response.Content);
 			}
 			else
 			{
