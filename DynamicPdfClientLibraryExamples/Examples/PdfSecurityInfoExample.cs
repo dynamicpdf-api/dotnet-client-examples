9dc497dd144b1b60aa90104a4437e38e9387c8ab
James A. Brannan <jamesabrannan@hushmail.com>
2025-11-03 10:55:43 -0500

    security info additions

---
+++ DynamicPdfClientLibraryExamples/Examples/PdfSecurityInfoExample.cs
@@ -0,0 +1,21 @@
+﻿
+using DynamicPDF.Api;
+using System;
+
+namespace DynamicPdfClientLibraryExamples.Examples
+{
+    public class PdfSecurityInfoExample
+    {
+        public static void Run(string apiKey, string basePath)
+        {
+            PdfResource resource = new PdfResource(basePath + "aes256-security.pdf");
+            PdfSecurityInfoEndpoint secInfoEndpoint = new PdfSecurityInfoEndpoint(resource);
+            secInfoEndpoint.ApiKey = apiKey;
+            PdfSecurityInfoResponse resp = secInfoEndpoint.Process();
+            PdfSecurityInfo info = resp.Content;
+            Console.WriteLine(resp.JsonContent);
+            Console.WriteLine("EncryptionType: " + info.EncryptionTypeString);
+
+        }
+    }
+}
---
+++ PdfSecurityInfoExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using DynamicPdfClientLibraryExamples.Examples;
+using DynamicPdfClientLibraryExamples.Utility;
+
+namespace PdfSecurityInfoProject
+{
+    class Program
+    {
+        private const string basePath = "../DynamicPdfClientLibraryExamples/Resources/security-info/";
+        private const string apiKey = ""DP--api-key--";
+
+        static void Main(string[] args)
+        {
+            PdfSecurityInfoExample.Run(apiKey, FileUtility.GetPath(basePath));
+        }
+    }
+}
